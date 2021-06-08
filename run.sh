005d9b24ccf49df9ced7db745134d91dad944c39
Qing Li <dylan.liqing@gmail.com>
2021-06-08 11:36:59 -0700

    Update

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 export vilt_data=/mnt/liqing/vilt_data/official_vilt_data
-python run.py with data_root=$vilt_data/VQAv2 num_gpus=1 num_nodes=1 per_gpu_batchsize=64 task_finetune_vqa_randaug test_only=True load_path="$vilt_data/vilt_vqa.ckpt"
+# python run.py with data_root=$vilt_data/VQAv2 num_gpus=1 num_nodes=1 per_gpu_batchsize=64 task_finetune_vqa_randaug test_only=True load_path="$vilt_data/vilt_vqa.ckpt"
+python run.py with data_root=$vilt_data/VQAv2 num_gpus=1 num_nodes=1 per_gpu_batchsize=64 task_finetune_vqa_trainval_randaug load_path="$vilt_data/vilt_200k_mlm_itm.ckpt"
